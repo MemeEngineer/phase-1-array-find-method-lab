5a5b8dbd419f08fd718be0aa9619893363bb36c7
Johnny Wu <johnnyw131@gmail.com>
2021-11-24 11:21:04 -0500

    commit tree 25ef689855bbc0dee554dccdab89aa383471582b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function superbowlWin (record) {
+    if (record.find(({result}) => result === 'N/A')){
+        return superbowlWin = undefined;
+    }
+ }
 
+ function superbowlWin (record) {
+     if (record.find(({result}) => result === 'W')){
+        return record.find(({result}) => result === 'W').year;
+        
+    }
+    }
+
+  
